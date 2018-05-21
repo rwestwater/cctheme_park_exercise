f9a85d60f80e12aa36b74516d4efcd283f7936ba
Rachel Westwater <rachel-westwater@hotmail.com>
2018-05-21 16:56:18 +0100

    basic get tests passing for ride class

--- src/main/java/Ride.java
+++ src/main/java/Ride.java
@@ -1,2 +1,34 @@
+import java.util.ArrayList;
+
 public class Ride {
+
+    private String name;
+    private String model;
+    private int minAge;
+    private int minHeight;
+
+    public Ride(String name, String model, int minAge, int minHeight){
+        this.name = name;
+        this.model = model;
+        this.minAge = minAge;
+        this.minHeight = minHeight;
+        }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getMinAge(){
+        return minAge;
+    }
+
+    public int getMinHeight() {
+        return minHeight;
+    }
+
+
 }
--- src/test/java/RideTest.java
+++ src/test/java/RideTest.java
@@ -1,2 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class RideTest {
+
+    Ride ride;
+
+    @Before
+    public void before(){
+        ride = new Ride("Nemasis", "Rollercoaster", 8, 135);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Nemasis", ride.getName());
+    }
+
+    @Test
+    public void hasModel(){
+        assertEquals("Rollercoaster", ride.getModel());
+    }
+
+    @Test
+    public void hasMinAge(){
+        assertEquals(8, ride.getMinAge());
+    }
+
+    @Test
+    public void hasMinHeight(){
+        assertEquals(135, ride.getMinHeight());
+    }
 }
