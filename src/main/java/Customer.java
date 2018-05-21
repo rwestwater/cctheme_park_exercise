a62b613c49cb4ec6079b315c773a9bf985137ebd
Rachel Westwater <rachel-westwater@hotmail.com>
2018-05-21 17:13:02 +0100

    basic get tests passing for customer class

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,2 +1,37 @@
 public class Customer {
+
+    private String name;
+    private String type;
+    private int age;
+    private int height;
+    private double money;
+
+    public Customer(String name, String type, int age, int height, double money){
+        this.name = name;
+        this.type = type;
+        this.age = age;
+        this.height = height;
+        this.money = money;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
 }
--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,2 +1,39 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CustomerTest {
+
+    Customer customer;
+
+    @Before
+    public void before(){
+        customer = new Customer("Rachel", "Adult", 25, 167, 50.00);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Rachel", customer.getName());
+    }
+
+    @Test
+    public void hasType(){
+        assertEquals("Adult", customer.getType());
+    }
+
+    @Test
+    public void hasAge(){
+        assertEquals(25, customer.getAge());
+    }
+
+    @Test
+    public void hasHeight(){
+        assertEquals(167, customer.getHeight());
+    }
+
+    @Test
+    public void hasMoney(){
+        assertEquals(50.00, customer.getMoney(), 0.01);
+    }
 }
